1fb11c5683db7fcfbe25e371374f7c3ee47b6817
atefeh97hmt <hemmati.atefeh97@gmail.com>
2025-04-06 19:36:34 +0430

    variables and operation

--- Kourosh-FrontClass/js/main.js
+++ Kourosh-FrontClass/js/main.js
@@ -2,15 +2,45 @@ console.log("hi javascript");
 console.error("this is error");
 console.warn("this is warning");
 
-var text = "hi ali"
+var text = "hi ali"  //string
 console.log(text);
 console.log(text.length);
 console.log(text.toUpperCase());
 
-var price = 1500;
-var price2 = "1500";
+var price = 1600;  //number
+var price2 = "1500";  //string
 
-var isOnline = true;
+var isOnline = true;  // boolean
 var isOnline = false;
+
+
+var x = 2;
+
+//  +
+//  -
+//  *
+//  /
+//  %
+//  **
+// =
+// <
+// >
+// <=
+// >=
+
+
+var a = 2 ;
+var b = 10 ;
+// var z = a + b;
+console.log (a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
+
+var name = "ali";
+var age = 25;
+var intro = ` I am ${name}, and my age is ${age} `;
+console.log(intro);
 
 
