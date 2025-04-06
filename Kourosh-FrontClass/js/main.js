e60646ef4dc38770c036a57c19d38e29c8300926
atefeh97hmt <hemmati.atefeh97@gmail.com>
2025-04-06 19:50:33 +0430

    if and else if

--- Kourosh-FrontClass/js/main.js
+++ Kourosh-FrontClass/js/main.js
@@ -44,3 +44,22 @@ var intro = ` I am ${name}, and my age is ${age} `;
 console.log(intro);
 
 
+// if condition
+
+var age2 = 15;
+if (age2 >= 18){
+    console.log("your age is valid");
+} else {
+    console.log("your age is invalid")
+};
+
+
+var score = 17;
+if (score > 17){
+    console.log("very good");
+} else if (score > 15){
+    console.log("good");
+}
+else{
+    console.log("bad")
+}
