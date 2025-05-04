5c40a271a2c47864aecd9bfe2d11c98c9afc9559
atefeh97hmt <hemmati.atefeh97@gmail.com>
2025-05-04 21:56:28 +0430

    array basics

---
+++ Reza-FrontClass/Javascript/main.js
@@ -0,0 +1,103 @@
+var x = 2 ;
+let a = 3;
+
+const b = 3.14;
+
+let name = "Reza";
+let age = 2;
+let isStudent = true;
+
+let fruits = ["apple", "banana", "orange", "5"];
+let numbers = [1, 2, 3, 4];
+
+let colors = new Array ("red", "blue", "green");
+let colorss = ["red", "blue", "green"];
+
+let cities = ["tehran", "shiraz", "alborz", "mashhad", "yazd"];
+console.log(cities);
+console.log("tehran");
+console.log(cities[0]);
+console.log(cities[3]);
+
+cities[1] = "isfahan";
+console.log(cities);
+
+console.log(cities.length);
+
+let myarr = [2, "sara", "mary", 5, 10];
+console.log(myarr);
+
+
+let nums = [1, 2, 3, 4, 5, 6];
+console.log(nums)
+
+nums.push(50);
+console.log(nums);
+
+
+nums.pop();
+console.log(nums);
+
+nums.unshift("sara");
+console.log(nums);
+
+nums.unshift("Reza");
+console.log(nums);
+
+nums.shift();
+console.log(nums);
+
+
+
+let students = ["ali", "sasan", "5698", "reza", "3"];
+console.log(students);
+
+students[2] = "10";
+console.log(students);
+
+students.shift();
+console.log(students);
+
+
+
+
+let myNumbers = [1, 8, 6, 7, 3, 2];
+console.log(myNumbers);
+
+myNumbers.sort();
+console.log(myNumbers);
+
+myNumbers.reverse();
+console.log(myNumbers);
+
+
+let anamial = ["bear", "dog", "cat"];
+console.log(anamial);
+let animalstr= anamial.join("-");
+console.log(animalstr);
+
+let str = "ali,reza,sara";
+console.log(str);
+let names = str.split(",");
+console.log(names);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
