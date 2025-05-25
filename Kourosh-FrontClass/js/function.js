e212743de7196e6c7e31e1361730d07af75c930e
atefeh97hmt <hemmati.atefeh97@gmail.com>
2025-05-25 19:53:29 +0430

    functions

---
+++ Kourosh-FrontClass/js/function.js
@@ -0,0 +1,44 @@
+// function name(params) {
+    
+// }
+
+
+function sayHello(){
+
+console.log("hello world!")
+
+}
+
+sayHello();
+
+
+// Drive: function() {
+//     this.currentRPM = 5000;
+//     }
+
+
+function greet(name){
+    console.log("hi"+ name )
+}
+greet ("koroush");
+
+
+
+function double(x){
+    return x * 2;
+}
+console.log(double(5));
+
+
+function isEven(num){
+
+if (num % 2 === 0){
+return true;
+} else{
+    return false;
+}
+}
+console.log(isEven(4));
+console.log(isEven(7));
+
+
