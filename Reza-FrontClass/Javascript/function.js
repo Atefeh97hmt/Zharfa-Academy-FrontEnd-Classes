6e78d4f5501a44b449dff225f6adab986ad3cc93
atefeh97hmt <hemmati.atefeh97@gmail.com>
2025-05-25 21:28:01 +0430

    function

---
+++ Reza-FrontClass/Javascript/function.js
@@ -0,0 +1,41 @@
+// function name(params) {
+    
+// }
+
+
+function sayHello(){
+    console.log("hellow world !")
+};
+
+sayHello();
+
+
+
+function greet(name){
+
+console.log("hi" + name + "!");
+
+}
+greet("Ali");
+greet("Reza");
+
+
+function double(x){
+    return x * 2;
+}
+console.log(double(5));
+
+
+function isEven(num){
+if (num % 2 === 0){
+
+    return true;
+}
+else {
+    return false;
+}
+}
+console.log( "this is an even number" + isEven(8));
+console.log(isEven(7));
+
+
