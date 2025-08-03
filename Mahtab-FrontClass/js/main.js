b8601a6fe0e05a539981b31cc71b21672e93c4bc
atefeh97hmt <hemmati.atefeh97@gmail.com>
2025-08-03 21:23:18 +0430

    javascript- data type

---
+++ Mahtab-FrontClass/js/main.js
@@ -0,0 +1,43 @@
+// alert('hello from internal javascript 222222!');
+
+
+// varriable
+var x = 2;
+const y = 3;
+
+var x = 5;
+
+// string
+var name = "mahtab";
+var num = "123";
+var num2 = '23';
+
+// number
+var age = 15;
+var height = 1.75;
+var score = -2;
+
+// boolean
+var isStudent = true;
+var isStudent = false;
+
+// undefined
+var xx;
+// xx = 6;
+
+console.log(xx);
+
+// array
+let fruits =['apple', 'banana', 'cherry'];
+console.log(fruits);
+
+// object
+
+var person ={
+    name: "mahtab",
+    age:13,
+    isStudent: true,
+};
+var isStudent = true;
+console.log(person.name);
+console.log(person.age);
