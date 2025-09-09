ed91e866ba41c803b3227aa38b1e7c0485cb35ec
atefeh97hmt <hemmati.atefeh97@gmail.com>
2025-09-09 20:10:55 +0430

    random change color

---
+++ Kourosh-FrontClass/js/final/BgColorChange1/main.js
@@ -0,0 +1,7 @@
+
+function changeColor(){
+    const colors = ['red', 'blue', 'green', 'yellow', 'pink', 'orange'];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor = randomColor;
+}
+
---
+++ Kourosh-FrontClass/js/final/BgColorChange2/main.js
@@ -0,0 +1,11 @@
+
+function changeColor(){
+    let letters = '123456789ABCDEF';
+    let color = "#"
+   for (let i = 0; i < 6; i++){
+    color += letters[Math.floor(Math.random() * 16)]
+   }
+
+   document.body.style.backgroundColor = color;
+
+}
