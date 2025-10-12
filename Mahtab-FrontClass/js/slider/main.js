926bde1d3cbba1bdb0724f03e4c208961063e0bd
atefeh97hmt <hemmati.atefeh97@gmail.com>
2025-10-12 20:10:03 +0330

    slider js

---
+++ Mahtab-FrontClass/js/slider/main.js
@@ -0,0 +1,26 @@
+const images = [
+    'https://picsum.photos/200/300',
+    'https://picsum.photos/id/237/200/300',
+    'https://picsum.photos/seed/picsum/200/300',
+];
+
+
+let current = 0;
+
+function showSlide(index) {
+    document.getElementById('slider').src = images[index];
+}
+
+
+function nextSlide() {
+    current++;
+    if (current >= images.length) current = 0;
+    showSlide(current);
+}
+
+
+function prevSlide() {
+    current--;
+    if (current < 0) current = images.length - 1;
+    showSlide(current);
+}
